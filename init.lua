a7d37ae91d7669f4147a4376471b8150a4fd1c29
jsjhy <32364249+jsjhy@users.noreply.github.com>
2024-05-02 19:44:41 +0800

    fix position of tabpages

--- init.lua
+++ init.lua
@@ -58,6 +58,14 @@ require("lazy").setup({
 	'akinsho/bufferline.nvim', 
 	config =function()
 	    require("bufferline").setup({
+		options = {
+		    offsets = {{
+			filetype = "NvimTree",
+			text = "File Explorer",
+			highlight = "Directory",
+			text_align = "left"
+		    }},
+		},
 		highlights = {
 		    tab_selected = {
 			fg = 'Grey',
